c7496f3e0573bdc903a05255303897da6f8d22fa
Pablo Prietz <pablo@prietz.org>
2021-11-03 13:06:04 +0100

    Use built-in PyInstaller signing on macOS

--- deployment/bundle_macos.sh
+++ deployment/bundle_macos.sh
@@ -1,19 +1,20 @@
 #!/usr/local/bin/zsh
 
-pl_codesign () {
-    sign="Developer ID Application: Pupil Labs UG (haftungsbeschrankt) (R55K9ESN6B)"
-    codesign \
-        --all-architectures \
-        --force \
-        --strict=all \
-        --options runtime \
-        --entitlements entitlements.plist \
-        --continue \
-        --verify \
-        --verbose=4 \
-        -s "$sign" \
-        --deep "$@"
-}
+sign="Developer ID Application: Pupil Labs UG (haftungsbeschrankt) (R55K9ESN6B)"
+
+# pl_codesign () {
+#     codesign \
+#         --all-architectures \
+#         --force \
+#         --strict=all \
+#         --options runtime \
+#         --entitlements entitlements.plist \
+#         --continue \
+#         --verify \
+#         --verbose=4 \
+#         -s "$sign" \
+#         --deep "$@"
+# }
 
 # get most major.minor tag, without trailing count
 current_tag=$(git describe --tags --long)
@@ -26,27 +27,36 @@ ext=app
 # bundle Pupil Capture
 printf "\n##########\nBundling Pupil Capture\n##########\n\n"
 cd deploy_capture
-./bundle.sh
+./bundle.sh \
+    --codesign-identity "$sign" \
+    --osx-entitlements-file entitlements.plist \
+    --target-arch x86_64
 mv dist/*.$ext ../$release_dir
 cd ..
 
 # bundle Pupil Service
 printf "\n##########\nBundling Pupil Service\n##########\n\n"
 cd deploy_service
-./bundle.sh
+./bundle.sh \
+    --codesign-identity "$sign" \
+    --osx-entitlements-file entitlements.plist \
+    --target-arch x86_64
 mv dist/*.$ext ../$release_dir
 cd ..
 
 # bundle Pupil Player
 printf "\n##########\nBundling Pupil Player\n##########\n\n"
 cd deploy_player
-./bundle.sh
+./bundle.sh \
+    --codesign-identity "$sign" \
+    --osx-entitlements-file entitlements.plist \
+    --target-arch x86_64
 mv dist/*.$ext ../$release_dir
 cd ..
 
-printf "\n##########\nSigning applications\n##########\n"
-pl_codesign $release_dir/*.$ext/Contents/Resources/**/.dylibs/*.dylib
-pl_codesign $release_dir/*.$ext
+# printf "\n##########\nSigning applications\n##########\n"
+# pl_codesign $release_dir/*.$ext/Contents/Resources/**/.dylibs/*.dylib
+# pl_codesign $release_dir/*.$ext
 
 printf "\n##########\nCreating dmg file\n##########\n"
 ln -s /Applications/ $release_dir/Applications
@@ -59,5 +69,5 @@ hdiutil create \
     -size "$size_in_b"b \
     $release_dir.dmg
 
-printf "\n##########\nSigning dmg file\n##########\n"
-pl_codesign "$release_dir.dmg"
+# printf "\n##########\nSigning dmg file\n##########\n"
+# pl_codesign "$release_dir.dmg"
